35a47684d67f7f020f0381847e3bc8dbe1d15de4
TJ Draper <tdraper@rxante.com>
2024-11-27 15:56:55 -0600

    Add phpstan standards

---
+++ phpstan.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+return [
+    'includes' => [
+        dirname(__DIR__, 2) . '/phpstan/phpstan-deprecation-rules/rules.neon',
+        dirname(__DIR__, 2) . '/phpstan/phpstan-strict-rules/rules.neon',
+        dirname(__DIR__, 2) . '/phpstan/phpstan-mockery/extension.neon',
+    ],
+];
