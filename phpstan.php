cc3c7514feeb005aebdd4ca2376c6a4d89808d57
TJ Draper <tdraper@rxante.com>
2024-12-10 14:42:19 -0600

    Add bnf/phpstan-psr-container

--- phpstan.php
+++ phpstan.php
@@ -7,5 +7,6 @@ return [
         dirname(__DIR__, 2) . '/phpstan/phpstan-deprecation-rules/rules.neon',
         dirname(__DIR__, 2) . '/phpstan/phpstan-strict-rules/rules.neon',
         dirname(__DIR__, 2) . '/phpstan/phpstan-mockery/extension.neon',
+        dirname(__DIR__, 2) . '/bnf/phpstan-psr-container/extension.neon',
     ],
 ];
