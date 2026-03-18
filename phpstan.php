92fccdb2328db80e2e96be1c12e8743c4a82ea9b
TJ Draper <tdraper@rxante.com>
2026-03-18 12:16:19 -0500

    Make PHPStand paths to required coding standards files more robust

--- phpstan.php
+++ phpstan.php
@@ -2,11 +2,27 @@
 
 declare(strict_types = 1);
 
+use Composer\InstalledVersions;
+
+/**
+ * We just need the path to a package we know is installed, and since this
+ * package also requires `doctrine/coding-standard` we can use that one.
+ * We can't use our own package name because it gets weird when testing as
+ * a symlinked package
+ */
+$doctrinePath = (string) InstalledVersions::getInstallPath(
+    'doctrine/coding-standard'
+);
+
+$doctrineRealPath = (string) realpath($doctrinePath);
+
+$vendorDir = dirname($doctrineRealPath, 2);
+
 return [
     'includes' => [
-        dirname(__DIR__, 2) . '/phpstan/phpstan-deprecation-rules/rules.neon',
-        dirname(__DIR__, 2) . '/phpstan/phpstan-strict-rules/rules.neon',
-        dirname(__DIR__, 2) . '/phpstan/phpstan-mockery/extension.neon',
-        dirname(__DIR__, 2) . '/bnf/phpstan-psr-container/extension.neon',
+        $vendorDir . '/phpstan/phpstan-deprecation-rules/rules.neon',
+        $vendorDir . '/phpstan/phpstan-strict-rules/rules.neon',
+        $vendorDir . '/phpstan/phpstan-mockery/extension.neon',
+        $vendorDir . '/bnf/phpstan-psr-container/extension.neon',
     ],
 ];
